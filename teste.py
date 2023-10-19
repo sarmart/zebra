afb6cb8627c38e1ce3496924ca446fc8d4d24f69
sarmart <128601286+sarmart@users.noreply.github.com>
2023-10-19 09:10:39 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,10 @@
+
+
+print ("digite um número inteiro positivo:")
+n = input
+
+if n < 0:
+    print ("nenhum número primo encontrado!")
+elif n % 2 == int:
+    for i in range(1,n+1):
+        if i % i == 0 and i % 1 == 0
